a1de112b1da9048c807c625a4726e42408ef2097
maoxin <2807109555@qq.com>
2022-07-01 17:49:21 +0800

    第一次提交

---
+++ src/main/java/com/redis/IndexController.java
@@ -0,0 +1,41 @@
+package com.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 测试节点挂了哨兵重新选举新的master节点，客户端是否能动态感知到
+     *
+     * @throws InterruptedException
+     */
+    @RequestMapping("/test_sentinel")
+    public void testSentinel() throws InterruptedException {
+        int i = 1;
+        while (true){
+            try {
+                stringRedisTemplate.opsForValue().set("maoxin"+i, i+""); //jedis.set(key,value);
+                System.out.println("设置key："+ "maoxin" + i);
+                i++;
+
+                if(i>30){
+                    continue;
+                }
+            }catch (Exception e){
+                logger.error("错误：", e);
+            }
+        }
+    }
+
+}
